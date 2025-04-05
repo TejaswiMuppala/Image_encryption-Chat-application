07adbfe05a7ceb195aba04c20d745a5139c7c9b7
TejaswiMuppala <bl.en.u4aie22059@bl.students.amrita.edu>
2025-04-05 19:21:23 +0530

    encryption-decryption

---
+++ README.md
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Add commit message"
+git push -u origin master
---
+++ test_bot.py
@@ -0,0 +1,17 @@
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+TOKEN = 'YOUR_BOT_TOKEN'
+
+def start(update, context):
+    update.message.reply_text('Hello!')
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
